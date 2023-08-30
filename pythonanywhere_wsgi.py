58e66c92f728b93094c9a3a41d987dfd7dc422b7
Florian Jörg <55591602+florianjoerg@users.noreply.github.com>
2023-08-30 12:17:31 +0200

    Create pythonanywhere_wsgi.py

---
+++ pythonanywhere_wsgi.py
@@ -0,0 +1,90 @@
+import sys
+import os
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/cluster-monitoring')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
+w_secret = os.environ["SECRET_KEY"]
+
+# add your project directory to the sys.path
+path = '/home/clustermonitoring/cluster-monitoring/tempmon'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ["TZ"] = "CEST" #to make datetime.strptime work...
+
+# need to pass the flask app as "application" for WSGI to work
+# for a dash app, that is at app.server
+# see https://plot.ly/dash/deployme
+from app import app
+import git
+from flask import request, abort
+import hmac
+import hashlib
+
+def is_valid_signature(x_hub_signature, data, private_key):
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
+
+@app.server.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method != 'POST':
+        return 'OK'
+    else:
+        abort_code = 418
+        # Do initial validations on required headers
+        if 'X-Github-Event' not in request.headers:
+            abort(abort_code)
+        if 'X-Github-Delivery' not in request.headers:
+            abort(abort_code)
+        if 'X-Hub-Signature' not in request.headers:
+            abort(abort_code)
+        if not request.is_json:
+            abort(abort_code)
+        if 'User-Agent' not in request.headers:
+            abort(abort_code)
+        ua = request.headers.get('User-Agent')
+        if not ua.startswith('GitHub-Hookshot/'):
+            abort(abort_code)
+
+        event = request.headers.get('X-GitHub-Event')
+        if event == "ping":
+            return json.dumps({'msg': 'Hi!'})
+        if event != "push":
+            return json.dumps({'msg': "Wrong event type"})
+
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        # webhook content type should be application/json for request.data to have the payload
+        # request.data is empty in case of x-www-form-urlencoded
+        if not is_valid_signature(x_hub_signature, request.data, w_secret):
+            print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
+            abort(abort_code)
+
+        payload = request.get_json()
+        if payload is None:
+            print('Deploy payload is empty: {payload}'.format(
+                payload=payload))
+            abort(abort_code)
+
+        if payload['ref'] != 'refs/heads/main':
+            return json.dumps({'msg': 'Not main; ignoring'})
+
+        repo = git.Repo('cluster-monitoring')
+        origin = repo.remotes.origin
+
+        pull_info = origin.pull()
+
+        if len(pull_info) == 0:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+        if pull_info[0].flags > 128:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+
+        commit_hash = pull_info[0].commit.hexsha
+        build_commit = f'build_commit = "{commit_hash}"'
+        print(f'{build_commit}')
+        return 'Updated PythonAnywhere server to commit {commit}'.format(commit=commit_hash)
+
+application = app.server
